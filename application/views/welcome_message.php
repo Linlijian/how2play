0f9bb99f7f0a5c281250ee1b682367fce1cb2b83
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-01-31 11:22:35 +0700

    fix แปป

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,7 +7,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>Welcome to How2play</title>
 </head>
 <body>
-
-
+	<div class="ui secondary  menu">
+	<a class="active item">
+		Home
+	</a>
+	<a class="item">
+		Messages
+	</a>
+	<a class="item">
+		Friends
+	</a>
+	<div class="right menu">
+		<div class="item">
+		<div class="ui icon input">
+			<input type="text" placeholder="Search...">
+			<i class="search link icon"></i>
+		</div>
+		</div>
+		<a class="ui item">
+		Logout
+		</a>
+	</div>
+	</div>
 </body>
 </html>
