0687b24b688e3fa572056bf0d23ce5858c6adad3
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-01-31 11:45:59 +0700

    index

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,29 +5,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to How2play</title>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>cssIndex/theme.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
 </head>
 <body>
-	<div class="ui secondary  menu">
-	<a class="active item">
-		Home
-	</a>
-	<a class="item">
-		Messages
-	</a>
-	<a class="item">
-		Friends
-	</a>
-	<div class="right menu">
-		<div class="item">
-		<div class="ui icon input">
-			<input type="text" placeholder="Search...">
-			<i class="search link icon"></i>
+	<div class="d-flex align-items-center section-aquamarine py-5 cover" style="background-image: url(&quot;assets/conference/cover.jpg&quot;);">
+		<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-white mt-5">
+			<h1 class="display-2 d-none d-md-block">How 2 Play</h1>
+			<h1 class="display-4 d-block d-md-none">Conference Pingendo</h1>
+			<p class="" style="">How to play, use, install and win</p>
+			<a href="#register" class="btn btn-lg mt-4 btn-outline-light">In Site</a> </div>
 		</div>
 		</div>
-		<a class="ui item">
-		Logout
-		</a>
-	</div>
 	</div>
 </body>
 </html>
---
+++ index2.php
@@ -0,0 +1,59 @@
+
+<html><head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- PAGE settings -->
+  <link rel="icon" href="https://templates.pingendo.com/assets/Pingendo_favicon.ico">
+  <title>Conference Aquamarine - Pingendo template</title>
+  <meta name="description" content="Free Bootstrap 4 Pingendo Aquamarine template for unique events.">
+  <meta name="keywords" content="Pingendo conference event aquamarine free template bootstrap 4">
+  <!-- CSS dependencies -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>cssIndex/theme.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <!-- Script: Make my navbar transparent when the document is scrolled to top -->
+  <script src="https://d33wubrfki0l68.cloudfront.net/js/12e3ca424c3c55e3d306e6423ed9e81f260dd657/aquamarine/js/navbar-ontop.js"></script>
+  <!-- Script: Animated entrance -->
+  <script src="https://d33wubrfki0l68.cloudfront.net/js/fe6311b3c294cba469a3939f21603640522c41e5/aquamarine/js/animate-in.js"></script>
+</head><body class="text-center">
+  <!-- Navbar -->
+  
+  <!-- Cover -->
+  <div class="d-flex align-items-center section-aquamarine py-5 cover" style="background-image: url(&quot;assets/conference/cover.jpg&quot;);">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-white mt-5">
+          <h1 class="display-2 d-none d-md-block">How 2 Play</h1>
+          <h1 class="display-4 d-block d-md-none">Conference Pingendo</h1>
+          <p class="" style="">December 12-14, NYC </p>
+          <a href="#register" class="btn btn-lg mt-4 btn-outline-light">เข้าสู่เว็บไซต์</a> </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- JavaScript dependencies -->
+  <div class="py-5"><div class="container"></div></div><script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <!-- Script: Smooth scrolling between anchors in a same page -->
+  <script src="https://d33wubrfki0l68.cloudfront.net/js/26a3094ac9c5e8d045f102e8929a6db58b3bbd83/aquamarine/js/smooth-scroll.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-29443235-15"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-29443235-15');
+</script><!-- Hotjar Tracking Code for https://templates.pingendo.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:713084,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+<pingendo onclick="window.open('https://pingendo.com/', '_blank')" style="cursor:pointer;position: fixed;bottom: 10px;right:10px;padding:4px;background-color: #00b0eb;border-radius: 8px; width:180px;display:flex;flex-direction:row;align-items:center;justify-content:center;font-size:14px;color:white">Made with Pingendo&nbsp;&nbsp;<img src="https://pingendo.com/site-assets/Pingendo_logo_big.png" class="d-block" alt="Pingendo logo" height="16"></pingendo></body></html>
