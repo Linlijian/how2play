f4987f5027705ae14014fff71bd419df9b64ee32
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-01-31 12:50:16 +0700

    add color index+link css semtic-ui

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,15 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<background="<?php echo base_url(); ?>img/index.jpg">
 	<title>Welcome to How2play</title>
 	<link rel="stylesheet" href="<?php echo base_url(); ?>cssIndex/theme.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>style/semantic.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+	<style>
+	body  {
+		background-image:url("<?php echo base_url(); ?>img/index.jpg");
+	}
+</style>
 </head>
 <body>
-	<div class="d-flex align-items-center section-aquamarine py-5 cover" style="background-image: url(&quot;assets/conference/cover.jpg&quot;);">
+	
+	<div class="d-flex align-items-center py-5 cover">
 		<div class="container">
 		<div class="row">
-			<div class="col-lg-12 text-white mt-5">
+			<div class="col-lg-12 text-bg mt-5">
 			<h1 class="display-2 d-none d-md-block">How 2 Play</h1>
 			<h1 class="display-4 d-block d-md-none">Conference Pingendo</h1>
 			<p class="" style="">How to play, use, install and win</p>
@@ -20,5 +28,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</div>
 		</div>
 	</div>
+
+
 </body>
 </html>
